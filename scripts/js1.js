b780b5c8b0fc5a724b9cc226bf009764dbdc9d9d
sashakisler <s.kisler@mail.ru>
2020-02-13 22:21:53 +0500

    da

--- scripts/js1.js
+++ scripts/js1.js
@@ -9,14 +9,12 @@ showBlock(selectors=n);
 function showBlock(n){
 var i;
 var slides = document.getElementsByClassName("select_items");
-var item = document.getElementsByClassName("image_item");
-var slides_image = document.getElementsByClassName("image_marker");
+var slides_image = document.getElementsByClassName("marker");
 for(i=0;i<slides.length;i++){
   slides[i].style.display="none";
+  slides_image[i].style.display="none";
 }
-for(i=0;i<slides.length;i++){
-    slides_image[i].style.display="none";
-  }
-      slides[selectors].style.display="block";
+
+      slides[selectors].style.display="flex";
       slides_image[selectors].style.display="block";
 }
