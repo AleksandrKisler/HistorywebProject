7c9118fa8e54e702073fdada915fb3bfd0ee9d2f
sashakisler <s.kisler@mail.ru>
2020-03-03 13:46:49 +0500

    add something more

--- scripts/js.js
+++ scripts/js.js
@@ -29,6 +29,73 @@ function showSlides(n) {
 }
 
 
+/*----------------
+SLIDER 1953
+----------------*/
+
+
+var slideIndex_1 = 1;
+  showSlides_1(slideIndex_1);
+
+function plusSlides_1(n) {
+  showSlides_1(slideIndex_1 += n);
+}
+
+function currentSlide_1(n) {
+  showSlides_1(slideIndex_1 = n);
+}
+
+
+
+
+function showSlides_1(n) {
+  var i;
+  var slides = document.getElementsByClassName("text_1");
+  var dots = document.getElementsByClassName("dot_1");
+  if (n > slides.length) {slideIndex_1 = slides.length;}
+  if (n < 1) {slideIndex_1 = 1;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex_1-1].style.display = "block";  
+  dots[slideIndex_1-1].className += " active";
+}
+
+var slideIndex_2 = 1;
+  showSlides_2(slideIndex_2);
+
+function plusSlides_2(n) {
+  showSlides_2(slideIndex_2 += n);
+}
+
+function currentSlide_2(n) {
+  showSlides_2(slideIndex_2 = n);
+}
+
+
+
+
+function showSlides_2(n) {
+  var i;
+  var slides = document.getElementsByClassName("text_2");
+  var dots = document.getElementsByClassName("dot_2");
+  if (n > slides.length) {slideIndex_2 = slides.length;}
+  if (n < 1) {slideIndex_2 = 1;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex_2-1].style.display = "block";  
+  dots[slideIndex_2-1].className += " active";
+}
+
+
+
 
  
 
@@ -71,8 +138,8 @@ function s(){
 /*modal window for Book*/
 
 var ModelButton_b =document.getElementsByClassName("button_forBook");
-var ModelWindow_b = document.getElementById("myModal1");
-var close_h=document.getElementsByClassName("p_close");
+var ModelWindow_b = document.getElementById("myModal2");
+var close_b=document.getElementsByClassName("p_close");
 
 function openWindowBook(){
     ModelWindow_b.style.display="block";
